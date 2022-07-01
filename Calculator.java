5c8cac4fb0bd87f94478e18e032a2b9d5106b233
gitnarayan <107856223+gitnarayan@users.noreply.github.com>
2022-07-02 02:26:20 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+
+
+import java.util.Scanner;
+
+public class Calculator {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter Number=");
+		
+		//Scanner s = new Scanner(System.in);
+		int a, b, sum=0, div=0;
+		
+		a = 12;
+		b = 10;
+		
+		
+		sum = a+b;
+		System.out.println("sum = "+sum);
+		
+		div = a-b;
+		System.out.println("div = "+div);
+	}
+
+}
